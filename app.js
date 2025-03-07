093da1c68566babd46ac1b6750c4b01532702e9b
Gigi <ginuscotto@gmail.com>
2025-03-06 23:34:04 -0300

    Se agrego funcion mostrarAmigos añadidos

--- app.js
+++ app.js
@@ -1,12 +1,22 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 var listaAmigos = [];
 
+
+function mostrarAmigos() {
+    let lista = document.getElementById("listaAmigos");
+    lista.innerHTML = "";
+    let pos = 0;
+    while(pos < listaAmigos.length){
+        lista.innerHTML += "<li>" + listaAmigos[pos] + "</li>";
+        pos++;
+    }    
+}
+
 function agregarALista(amigo){
     listaAmigos.push(amigo);
     alert(listaAmigos);
 
 }
-
 
 function validarAmigo(amigo) {
     if (amigo != ""){
@@ -26,4 +36,5 @@ function agregarAmigo() {
     let amigoUsuario = document.getElementById("amigo").value;
     validarAmigo(amigoUsuario);
     limpiarCampo("amigo");
+    mostrarAmigos();
 }
